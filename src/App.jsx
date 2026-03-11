6ba6b5a81b0b46f7b878f0d5735778683eddae36
LaloTheDrug <kiet0966923001@gmail.com>
2026-03-11 08:47:09 +0700

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
 import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
-/* AUTH PAGES */
-import Login from "./pages/auth/Login";
-import ForgotPassword from "./pages/auth/ForgotPassword";
-import ResetPassword from "./pages/auth/ResetPassword";
-
-/* STUDENT PAGES */
-import StudentLayout from "./layouts/student/StudentLayout";
-import StudentDashboard from "./pages/student/dashboard/StudentDashboard";
-import StudentClasses from "./pages/student/classes/StudentClasses";
-import StudentGrades from "./pages/student/grades/StudentGrades";
-import StudentQuiz from "./pages/student/quiz/StudentQuiz";
-import StudentNotifications from "./pages/student/notification/StudentNotifications";
-import StudentSchedule from "./pages/student/schedule/StudentSchedule";
-import StudentSupport from "./pages/student/support/StudentSupport";
-import StudentProfile from "./pages/student/profile/StudentProfile";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
 /* GLOBAL STYLE */
 import "./styles/variables.css";
@@ -24,30 +9,7 @@ import "./styles/global.css";
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-
-                {/* DEFAULT */}
-                <Route path="/" element={<Navigate to="/login" replace />} />
-
-                {/* AUTH */}
-                <Route path="/login" element={<Login />} />
-                <Route path="/login/forgotpass" element={<ForgotPassword />} />
-                <Route path="/login/resetpass" element={<ResetPassword />} />
-
-                {/* STUDENT */}
-                <Route path="/student" element={<StudentLayout />}>
-                    <Route index element={<Navigate to="dashboard" replace />} />
-                    <Route path="dashboard" element={<StudentDashboard />} />
-                    <Route path="classes" element={<StudentClasses />} />
-                    <Route path="grades" element={<StudentGrades />} />
-                    <Route path="quiz" element={<StudentQuiz />} />
-                    <Route path="notifications" element={<StudentNotifications />} />
-                    <Route path="schedule" element={<StudentSchedule />} />
-                    <Route path="support" element={<StudentSupport />} />
-                    <Route path="profile" element={<StudentProfile />} />
-                </Route>
-
-            </Routes>
+            <AppRoutes />
         </BrowserRouter>
     );
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,46 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
+/* AUTH PAGES */
+import Login from "../pages/auth/Login";
+import ForgotPassword from "../pages/auth/ForgotPassword";
+import ResetPassword from "../pages/auth/ResetPassword";
+
+/* STUDENT PAGES */
+import StudentLayout from "../layouts/student/StudentLayout";
+import StudentDashboard from "../pages/student/dashboard/StudentDashboard";
+import StudentClasses from "../pages/student/classes/StudentClasses";
+import StudentGrades from "../pages/student/grades/StudentGrades";
+import StudentQuiz from "../pages/student/quiz/StudentQuiz";
+import StudentNotifications from "../pages/student/notification/StudentNotifications";
+import StudentSchedule from "../pages/student/schedule/StudentSchedule";
+import StudentSupport from "../pages/student/support/StudentSupport";
+import StudentProfile from "../pages/student/profile/StudentProfile";
 
 export default function AppRoutes() {
-  return (
-    <Routes>
-    </Routes>
-  );
+    return (
+        <Routes>
+
+            {/* DEFAULT */}
+            <Route path="/" element={<Navigate to="/login" replace />} />
+
+            {/* AUTH */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/login/forgotpass" element={<ForgotPassword />} />
+            <Route path="/login/resetpass" element={<ResetPassword />} />
+
+            {/* STUDENT */}
+            <Route path="/student" element={<StudentLayout />}>
+                <Route index element={<Navigate to="dashboard" replace />} />
+                <Route path="dashboard" element={<StudentDashboard />} />
+                <Route path="classes" element={<StudentClasses />} />
+                <Route path="grades" element={<StudentGrades />} />
+                <Route path="quiz" element={<StudentQuiz />} />
+                <Route path="notifications" element={<StudentNotifications />} />
+                <Route path="schedule" element={<StudentSchedule />} />
+                <Route path="support" element={<StudentSupport />} />
+                <Route path="profile" element={<StudentProfile />} />
+            </Route>
+
+        </Routes>
+    );
 }
